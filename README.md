296f53a1841ab5b76f48f324ce9903bb61a32c0f
Joerg Kastning <jkastning@my-it-brain.de>
2024-09-08 21:10:15 +0200

    Add: Info to README and first build script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # container-image-forge
-This is a place where OCI container images are forged for fun and learning opportunities
+This is a place where [OCI](https://de.wikipedia.org/wiki/Open_Container_Initiative) container images are forged for fun and learning opportunities.
+
+In this repository you will find the Bash scripts that I use to build OCI compatible container images. The scripts contain the `buildah` commands that I use to create and customize the resulting images.
+
+If you stumbled upon this repository by accident and would like to learn more on the topic, or just want to know why I use `buildah` instead of `podman build` or the Docker CLI, see the following links for starters:
+
+  * [https://podman.io/](https://podman.io/)
+  * [https://buildah.io/](https://buildah.io/)
+  * [Buildah and Podman Relationship](https://podman.io/blogs/2018/10/31/podman-buildah-relationship.html)
+
+To me `buildah` seems to be the right tool to use when you are going to create container images. And as I need some customized images for work and weekend project the time seems due to learn how to use it.
+
+In case you spot any issues in my scripts or see something that could be improved, please let me know using the issue tracker of this repo or send a Pull Request.
---
+++ buildah_create_debian_bookworm_with_rootless_podman.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+# Name: buildah_create_debian_bookworm_with_rootless_podman.sh
+# Description:
+# Build an OCI compliant container image serving rootless podman on
+# Debian 12 (Bookworm)
+#
+# CAUTION: This is highly experimental and might breack your system!
+#          Use at your own risk!
+#
+# Author: Joerg Kastning
+# License: GPLv3-or-later
+
+# set -x
+set -euo pipefail
+
+if ! buildah -v &> /dev/null
+then
+  echo "The command buildah could not be found"
+  exit 1
+fi
+
+# Get a base image
+ctr=$(buildah from docker://docker.io/library/debian:bookworm)
+
+buildah run -- $ctr apt -y update
+buildah run -- $ctr apt -y upgrade
+buildah run -- $ctr apt -y install podman fuse-overlayfs libvshadow-utils libcap2-bin ca-certificates
+#buildah run -- $ctr apt -y clean
+#buildah run -- $ctr rm -rf /var/lib/apt/lists/*
+
+buildah run -- $ctr useradd podman
+buildah run -- $ctr sh -c "echo podman:1:999 > /etc/subuid"
+buildah run -- $ctr sh -c "echo podman:1001:64535 >> /etc/subuid"
+buildah run -- $ctr sh -c "echo podman:1:999 > /etc/subgid"
+buildah run -- $ctr sh -c "echo podman:1001:64535 >> /etc/subgid"
+buildah run -- $ctr setcap cap_setuid+epi /usr/bin/newuidmap
+buildah run -- $ctr setcap cap_setgid+epi /usr/bin/newgidmap
+ 
+buildah config -v /var/lib/containers $ctr
+buildah config -v /home/podman/.local/share/containers $ctr
+buildah run -- $ctr chown -R podman:podman /home/podman
+buildah run -- $ctr sh -c "mkdir -p /var/lib/shared/overlay-images /var/lib/shared/overlay-layers /var/lib/shared/vfs-images /var/lib/shared/vfs-layers; touch /var/lib/shared/overlay-images/images.lock; touch /var/lib/shared/overlay-layers/layers.lock; touch /var/lib/shared/vfs-images/images.lock; touch /var/lib/shared/vfs-layers/layers.lock"
+buildah config --env _CONTAINERS_USERNS_CONFIGURED="" $ctr
+buildah run -- $ctr apt -y reinstall uidmap
+ 
+buildah commit $ctr debian_bookworm_podman
